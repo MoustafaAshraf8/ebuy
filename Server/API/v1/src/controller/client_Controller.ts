8febc5e4ba09be93fa29ecd3bdb6f8b998b40133
MoustafaAshraf8 <mostafa.ashraf8642@gmail.com>
2023-07-29 18:43:15 +0300

    login jwt

--- Server/API/v1/src/controller/client_Controller.ts
+++ Server/API/v1/src/controller/client_Controller.ts
@@ -3,7 +3,8 @@ import { Client_Interface } from "../Interface/Client_Interface.js";
 import { Client_signIn_Interface } from "../Interface/Client_signIn_Interface.js";
 import { Error_message_Interface } from "../Interface/Error_message_Interface.js";
 import { Cart_product_Interface } from "../Interface/Cart_product_Interface.js";
-import { addClient } from "../service/client_Service.js";
+import { clientSignUp_service } from "../service/client_Service.js";
+import { clientLogIn_service } from "../service/client_Service.js";
 import { authenticateClient } from "../service/client_Service.js";
 import { addToClientCart } from "../service/client_Service.js";
 import { JWT_Class } from "../../utilities/JWT_Class.js";
@@ -22,9 +23,8 @@ const clientSignUp = async (
   };
 
   //step-1 add to db
-  let result: Client_Interface | Error_message_Interface = await addClient(
-    newClientData
-  );
+  let result: Client_Interface | Error_message_Interface =
+    await clientSignUp_service(newClientData);
 
   //step-2 generate jwt
   if ("id" in result) {
@@ -51,14 +51,13 @@ const clientSignIn = async (
 
   //step-1 authenticate
   let result: Client_Interface | Error_message_Interface =
-    await authenticateClient(oldClientData);
+    await clientLogIn_service(oldClientData);
 
   //step-2 generate jwt
   if ("id" in result) {
     let id = result.id;
     let accessToken = JWT_Class.createAccessToken(String(id));
-    let refreshToken = JWT_Class.createRefreshToken(String(id));
-    res.cookie("refreshCookie", refreshToken, {
+    res.cookie("accessCookie", accessToken, {
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000,
     });
--- Server/API/v1/src/service/client_Service.ts
+++ Server/API/v1/src/service/client_Service.ts
@@ -3,13 +3,19 @@ import { Client_Interface } from "../Interface/Client_Interface.js";
 import { Client_signIn_Interface } from "../Interface/Client_signIn_Interface.js";
 import { Error_message_Interface } from "../Interface/Error_message_Interface.js";
 
-const addClient = async (
+const clientSignUp_service = async (
   client: Client_Interface
 ): Promise<Client_Interface | Error_message_Interface> => {
   let newClient = new Client(client);
   let clientData = await newClient.signUp();
   console.log(clientData);
   return clientData;
+};
+const clientLogIn_service = async (
+  client: Client_signIn_Interface
+): Promise<Client_Interface | Error_message_Interface> => {
+  let result = await Client.signIn(client);
+  return result;
 };
 
 const authenticateClient = async (
@@ -33,4 +39,9 @@ const addToClientCart = async (
   return result;
 };
 
-export { addClient, authenticateClient, addToClientCart };
+export {
+  clientSignUp_service,
+  clientLogIn_service,
+  authenticateClient,
+  addToClientCart,
+};
